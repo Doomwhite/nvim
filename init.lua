45eb75ef101f65cfd42b1b45a248daa30ed648b7
Doomwhite <doomwhitex@gmail.com>
2023-04-09 17:51:59 -0300

    Makes the starting folders

--- init.lua
+++ init.lua
@@ -1,2 +1,16 @@
-require("doomwhite")
+command = vim.api.nvim_create_user_command
+ag = vim.api.nvim_create_augroup
+au = vim.api.nvim_create_autocmd
 
+require 'options'
+require 'commands'
+require 'aucmd'
+require 'mappings'
+
+require 'lazy'.setup('plugins', {
+	dev = {
+		path = '~/Developer',
+		patterns = {'bennypowers'},
+		fallback = true,
+	}
+})
